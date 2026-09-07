3ec6c2cfb21e54c3fb2f7baaa588977f7a0c60c2
SAM KAI RUI <samkairui@SAMs-MacBook-Air.local>
2026-09-07 18:15:54 +0800

    7/9/2026

--- fraud-detection-service/src/main/java/com/banking/frauddetectionservice/service/FraudDetectionService.java
+++ fraud-detection-service/src/main/java/com/banking/frauddetectionservice/service/FraudDetectionService.java
@@ -4,12 +4,17 @@ import com.banking.frauddetectionservice.client.AccountServiceClient;
 import com.banking.frauddetectionservice.model.FraudCheckResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
@@ -20,6 +25,13 @@ public class FraudDetectionService {
 
     private final AccountServiceClient accountServiceClient;
     private final KafkaTemplate<String,Object> kafkaTemplate;
+    private final RedisTemplate<String,String> redisTemplate;
+
+    @Value("${fraud.max-transaction-per-minute}")
+    private int maxTransactionsPerMinute;
+
+    @Value("{fraud.suspicious-amount-multiplier}")
+    private double suspiciousAmountMultiplier;
 
     private static final String VERIFICATION_REQUIRED_TOPIC = "verification.required";
     private static final String FRAUD_CHECK_CLEAN_RESULT_TOPIC = "fraud.check.clean";
@@ -68,7 +80,7 @@ public class FraudDetectionService {
         // Fraud Check Pattern 1: Velocity Check
         if(isVelocityExceeded(accountNumber)){
             return new FraudCheckResult(
-                    true,"too many transaction in 60 seconds" + "- Velocity limit exceeded"
+                    true,"Too many transaction in 60 seconds" + "- Velocity limit exceeded"
             );
         }
         // Fraud Check Pattern 2: Amount Check
@@ -79,11 +91,49 @@ public class FraudDetectionService {
         }
 
         // Fraud Check Pattern 3: Balance Check
-        if(senderBalance.compareTo(BigDecimal.ZERO) && is){
+        if(senderBalance.compareTo(BigDecimal.ZERO) > 0 && isBalanceCheckFailed(senderBalance,amount)){
             return new FraudCheckResult(
-                    true,"Unusual transaction amount" + "- Exceeds 3x your average"
-            );
+                    true,"Transaction exceed 90% of account balance");
         }
+
+        return new FraudCheckResult(false,null);
+    }
+
+
+    private boolean isVelocityExceeded(String accountNumber){
+        String key = "fraud:velocity" + accountNumber;
+        Long count = redisTemplate.opsForValue().increment(key);
+
+        if(count != null && count == 1){
+            redisTemplate.expire(key, Duration.ofMinutes(1));
+        }
+        log.info("Velocity check - account: {} count : {}/{}",accountNumber,count,maxTransactionsPerMinute);
+
+        return count != null && count > maxTransactionsPerMinute;
+    }
+
+    private boolean isAmountSuspicious(String accountNumber,BigDecimal amount){
+        String avgKey = "fraud:avg_amount" + accountNumber;
+        String avgStr = redisTemplate.opsForValue().get(avgKey);
+        if (avgStr == null){
+            redisTemplate.opsForValue().set(avgKey,amount.toString());
+            return false;
+        }
+        BigDecimal avgAmount = new BigDecimal(avgStr);
+        BigDecimal threshold = avgAmount.multiply(
+                BigDecimal.valueOf(suspiciousAmountMultiplier)
+        );
+
+        // update running average
+        BigDecimal newAvg = avgAmount.add(amount).divide(BigDecimal.valueOf(2),2, RoundingMode.HALF_UP);
+
+        redisTemplate.opsForValue().set(avgKey,newAvg.toString());
+
+        log.info("Amount check - check: {} threshold: {} suspicious: {}",amount,threshold,amount.compareTo(threshold) > 0);
+    }
+
+    private boolean isBalanceCheckFailed(BigDecimal senderBalance,BigDecimal amount){
+
     }
 }
 
